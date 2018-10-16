d0b816a30c3668d11fce2efa4cea479c152c023b
Sebastian Civarolo <scivarolo@gmail.com>
2018-10-16 15:28:01 -0500

    Starter files for student components exercise

---
+++ 1-student-components/scripts.js
@@ -0,0 +1,74 @@
+const students = [
+  {
+      name: "Chris Miller",
+      class: "History",
+      info: "Failed last exam",
+      score: 59
+  },
+  {
+      name: "Courtney Seward",
+      class: "History",
+      info: "Has completed all homework",
+      score: 91
+  },
+  {
+      name: "Garrett Ward",
+      class: "History",
+      info: "Wonderful at helping other students",
+      score: 88
+  },
+  {
+      name: "John Dulaney",
+      class: "History",
+      info: "Has never missed a class or exam",
+      score: 92
+  },
+  {
+      name: "Greg Lawrence",
+      class: "History",
+      info: "Sub-par performance all around",
+      score: 64
+  },
+  {
+      name: "Leah Duvic",
+      class: "History",
+      info: "Wonderful student",
+      score: 97
+  },
+  {
+      name: "Jesse Page",
+      class: "History",
+      info: "Smokes too much. Distracting.",
+      score: 76
+  },
+  {
+      name: "Kevin Haggerty",
+      class: "History",
+      info: "Falls asleep in class",
+      score: 79
+  },
+  {
+      name: "Max Wolf",
+      class: "History",
+      info: "Talks too much",
+      score: 83
+  },
+  {
+      name: "Lissa Goforth",
+      class: "History",
+      info: "Asks pointless, unrelated questions",
+      score: 78
+  },
+  {
+      name: "Tyler Bowman",
+      class: "History",
+      info: "When was the last time he attended class?",
+      score: 48
+  },
+  {
+      name: "Ray Medrano",
+      class: "History",
+      info: "Needs to contribute to in-class discussions",
+      score: 95
+  }
+]
