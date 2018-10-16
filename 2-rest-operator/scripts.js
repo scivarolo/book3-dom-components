5bd9398aca64cae9edfc5c2b93baca3bda1f885a
Sebastian Civarolo <scivarolo@gmail.com>
2018-10-16 18:16:18 -0500

    fixed formatting issue in both script files

--- 2-rest-operator/scripts.js
+++ 2-rest-operator/scripts.js
@@ -1,75 +1,74 @@
-const students = [
-  {
-      name: "Chris Miller",
-      class: "History",
-      info: "Failed last exam",
-      score: 59
+const students = [{
+    name: "Chris Miller",
+    class: "History",
+    info: "Failed last exam",
+    score: 59
   },
   {
-      name: "Courtney Seward",
-      class: "History",
-      info: "Has completed all homework",
-      score: 91
+    name: "Courtney Seward",
+    class: "History",
+    info: "Has completed all homework",
+    score: 91
   },
   {
-      name: "Garrett Ward",
-      class: "History",
-      info: "Wonderful at helping other students",
-      score: 88
+    name: "Garrett Ward",
+    class: "History",
+    info: "Wonderful at helping other students",
+    score: 88
   },
   {
-      name: "John Dulaney",
-      class: "History",
-      info: "Has never missed a class or exam",
-      score: 92
+    name: "John Dulaney",
+    class: "History",
+    info: "Has never missed a class or exam",
+    score: 92
   },
   {
-      name: "Greg Lawrence",
-      class: "History",
-      info: "Sub-par performance all around",
-      score: 64
+    name: "Greg Lawrence",
+    class: "History",
+    info: "Sub-par performance all around",
+    score: 64
   },
   {
-      name: "Leah Duvic",
-      class: "History",
-      info: "Wonderful student",
-      score: 97
+    name: "Leah Duvic",
+    class: "History",
+    info: "Wonderful student",
+    score: 97
   },
   {
-      name: "Jesse Page",
-      class: "History",
-      info: "Smokes too much. Distracting.",
-      score: 76
+    name: "Jesse Page",
+    class: "History",
+    info: "Smokes too much. Distracting.",
+    score: 76
   },
   {
-      name: "Kevin Haggerty",
-      class: "History",
-      info: "Falls asleep in class",
-      score: 79
+    name: "Kevin Haggerty",
+    class: "History",
+    info: "Falls asleep in class",
+    score: 79
   },
   {
-      name: "Max Wolf",
-      class: "History",
-      info: "Talks too much",
-      score: 83
+    name: "Max Wolf",
+    class: "History",
+    info: "Talks too much",
+    score: 83
   },
   {
-      name: "Lissa Goforth",
-      class: "History",
-      info: "Asks pointless, unrelated questions",
-      score: 78
+    name: "Lissa Goforth",
+    class: "History",
+    info: "Asks pointless, unrelated questions",
+    score: 78
   },
   {
-      name: "Tyler Bowman",
-      class: "History",
-      info: "When was the last time he attended class?",
-      score: 48
+    name: "Tyler Bowman",
+    class: "History",
+    info: "When was the last time he attended class?",
+    score: 48
   },
   {
-      name: "Ray Medrano",
-      class: "History",
-      info: "Needs to contribute to in-class discussions",
-      score: 95
+    name: "Ray Medrano",
+    class: "History",
+    info: "Needs to contribute to in-class discussions",
+    score: 95
   }
 ]
 
@@ -90,7 +89,7 @@ const aside = (title, style) => `<aside class="${style}">${title}</aside>`;
 
 for (student of students) {
   let studentComponent = `<div class="student">`;
-  
+
   if (student.score >= 60) {
     studentComponent += `
       ${h1(student.name, "xx-large passing")}
@@ -105,11 +104,7 @@ for (student of students) {
     `;
   }
   studentComponent += `</div>`;
+  
+  // Add each student to the container element
   container.innerHTML += studentComponent;
 }
-
-/* <div class="student">
-  <h1 class="xx-large green">Student Name</h1>
-  <section class="bordered dashed section--padded">Student class</section>
-  <aside class="pushRight">Additional information</aside>
-</div> */
